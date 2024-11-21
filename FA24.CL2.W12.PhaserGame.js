37c24562134fe19fbd93782fb596e95141a309f1
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-11-21 12:19:04 -0500

    added placeholder scenes

--- FA24.CL2.W12.PhaserGame.js
+++ FA24.CL2.W12.PhaserGame.js
@@ -1,4 +1,11 @@
 window.onload = function(){
     //var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-    var game = new Phaser.Game();
+    var config = {
+        width: 800,
+        height: 600,
+        backgroundColor: 0x000000,
+        scene: [Scene1, Scene2],
+    }
+    
+    var game = new Phaser.Game(config);
 }
