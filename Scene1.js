8377bde0ea415bfa81e724fa18ec2d8324d27c26
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-11-21 13:06:43 -0500

    added some images, but they are not loading
    need to setup web server to bypass browser security restrictions. Need more time to proceed.

--- Scene1.js
+++ Scene1.js
@@ -3,6 +3,11 @@ class Scene1 extends Phaser.Scene{
         super("bootGame");
     }
     
+    preload(){
+        this.load.image("background", "./assets/images/background.png");
+        this.load.image("ship", "./assets/images/ship.png");
+        
+    }
     create(){
         this.add.text(20, 20, "Loading game...");
         this.scene.start("playGame");
--- Scene2.js
+++ Scene2.js
@@ -4,6 +4,11 @@ class Scene2 extends Phaser.Scene{
     }
     
     create(){
+        this.background = this.add.image(200, 100, "background");
+        this.background.setOrigin(0, 0);
+        
+        this.ship1 = this.add.image(this.width/2 - 50, this.height/2, "ship");
+        
         this.add.text(20, 20, "Playing game...", {font: "25px Arial", fill: "purple"});
     }
 }
